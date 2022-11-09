a7dc1e952d485377bbf91d7482daf53291c89d2f
tkcannon <taylorkcannon@gmail.com>
2022-11-08 20:50:59 -0700

    undid changes to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Theme from "./Theme";
 import Header from "./Components/Header";
